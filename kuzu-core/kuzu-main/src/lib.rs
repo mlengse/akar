ce2fd74899d90a9691a39ebcad2415cd197a6c40
mlengse <anjangkn@gmail.com>
2026-07-19 01:58:14 +0700

    feat: add WAL dump tool and expose low-level storage metadata via StorageDriver API

--- kuzu-core/kuzu-main/src/lib.rs
+++ kuzu-core/kuzu-main/src/lib.rs
@@ -4,6 +4,7 @@ pub mod connection;
 pub mod database;
 pub mod prepared_statement;
 pub mod query_result;
+pub mod storage_driver;
 
 #[cfg(feature = "adbc")]
 pub mod adbc;
@@ -15,3 +16,4 @@ pub use connection::Connection;
 pub use database::{Database, SystemConfig};
 pub use prepared_statement::PreparedStatement;
 pub use query_result::QueryResult;
+pub use storage_driver::StorageDriver;
