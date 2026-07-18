d58eb99695a0ce5fff19943de0888cd38d5e1916
mlengse <anjangkn@gmail.com>
2026-07-18 20:15:31 +0700

    feat: implement binder for query binding and add initial integration test suite

--- kuzu-core/kuzu-binder/src/binder/mod.rs
+++ kuzu-core/kuzu-binder/src/binder/mod.rs
@@ -1142,11 +1142,6 @@ impl Binder {
     fn bind_union(&self, u: kuzu_parser::ast::UnionStatement) -> Result<BoundStatement, String> {
         let left = self.bind_query(u.left)?;
         let right = self.bind_query(u.right)?;
-        if let (BoundStatement::BoundQuery(lq), BoundStatement::BoundQuery(rq)) = (&left, &right) {
-            if lq.clauses.len() != rq.clauses.len() {
-                return Err("UNION queries must have compatible structures".into());
-            }
-        }
         Ok(BoundStatement::BoundUnion(BoundUnion {
             left: Box::new(match left {
                 BoundStatement::BoundQuery(q) => q,
